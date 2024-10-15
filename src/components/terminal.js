baa20143850b7304826516548bb2063592f0583a
SuperMicah585 <micahphlps@gmail.com>
2024-10-14 21:00:50 -0700

    query applies to text box

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -7,6 +7,7 @@ const Terminal = forwardRef((_, ref) => {
     const [splitOnPeriodArray,setSplitOnPeriodArray] = useState([])
     const [position, setPosition] = useState({ x: 0, y: 0 });
     const [originalPosition] = useState({ x: 0, y: 0 }); // Store the original position
+    const [isInputHovered,setIsInputHovered] = useState(false)
     //const[highlightedText, setHighlightedText] = useState('')
 
 
@@ -20,7 +21,7 @@ const Terminal = forwardRef((_, ref) => {
 
     const tailwindColors = [
 
-        'text-fuchsia-500 font-bold','text-indigo-200 font-bold', 'text-green-200 font-bold','text-cyan-500 font-bold'
+        'text-fuchsia-300 font-bold','text-indigo-400 font-bold', 'text-green-200 font-bold','text-cyan-500 font-bold'
 
     ]
 
@@ -31,7 +32,9 @@ const Terminal = forwardRef((_, ref) => {
 
 
 const queryItems = [
-    'Projects','Hobbies','Tech Stack', 'Education','Work Experience'
+    {text:'Projects',endpoint:'client.micah.projects.list()'},{text:'Hobbies',endpoint:'client.micah.hobbies.list()'},
+    {text:'Tech Stack',endpoint:'client.micah.tech_stack.list()'}, {text:'Education',endpoint:'client.micah.education.list()'},
+    {text:'Work Experience',endpoint:'client.micah.work_experience.list()'}
 
 
 ]
@@ -54,16 +57,53 @@ const handleStop = (e, data) => {
       draggedRect.top < inputRect.bottom     // Top edge of draggable is before the bottom edge of target
     ){
       // Update the input value with the dragged element's innerHTML
-   
-      setInputValue(draggedElement.innerHTML);
+      //console.log(draggedElement.getAttribute('data-value'))
+      setInputValue(draggedElement.getAttribute('data-value'));
     }
     setPosition(originalPosition);
+    setIsInputHovered(false)
+  }
+
+
+  const handleDrag = (e, data) => {
+    const inputElement = document.getElementById('inputField');
+    const inputRect = inputElement.getBoundingClientRect();
+
+    // Use e.target to refer to the dragged element
+    const draggedElement = e.target;
+   
+    const draggedRect = e.target.getBoundingClientRect(); // Draggable element's bounding box
+
+
+    // Check if there's an overlap (or if they're touching)
+    if (
+      draggedRect.right > inputRect.left &&  // Right edge of draggable is past the left edge of target
+      draggedRect.left < inputRect.right &&  // Left edge of draggable is before the right edge of target
+      draggedRect.bottom > inputRect.top &&  // Bottom edge of draggable is past the top edge of target
+      draggedRect.top < inputRect.bottom     // Top edge of draggable is before the bottom edge of target
+    ){
+      // Update the input value with the dragged element's innerHTML
+      setIsInputHovered(true)
+ 
+    }
+    else{
+        setIsInputHovered(false)
+    }
+    
+  }
+
+  const handleSubmitClick = ()=>{
+
+    const inputElement = document.getElementById('inputField')
+    const value = inputElement.getAttribute('value')
+    console.log(value)
+
   }
 
 return(
     <>
        <div className = 'p-12 bg-zinc-900'> </div>
-        <div ref={ref} className = 'flex relative flex-col h-content bg-zinc-900 p-10'>
+        <div className = 'flex relative flex-col h-content bg-zinc-900 p-10'>
             <div className = 'absolute text-opacity-75 font-semibold ml-10 top-0 text-yellow-300 z-50 text-6xl font-termina'>ABOUT ME</div>
 
             <div className = 'mt-16 ml-10 w-[80%] h-10'>
@@ -80,22 +120,26 @@ return(
             
             </div>
 
+            <div className = 'flex justify-center items-center gap-5'>
             <input
+                spellcheck="false"
                 id='inputField'
                 type="text" 
                 placeholder="Type or Drag Your Query"
-                className='w-full z-50 h-full text-transparent h-8 bg-zinc-800 font-termina pl-2 p-1 rounded-lg'
+                className={`w-full border-4 ${isInputHovered ? 'border-blue-500' : 'border-transparent'} h-full text-transparent font-bold h-8 bg-zinc-800 font-termina p-2 rounded-lg caret-blue-500`}
                 value={inputValue}
                 onChange={handleChange}
             />
+            <div onClick={handleSubmitClick} className='flex items-center font-semibold cursor-pointer text-blue-500 border-blue-500 border-2 justify-center p-1 hover:bg-blue-500 hover:text-slate-200 rounded-lg'>submit</div>
+            </div>
 
             </div>
             <div className = 'flex h-screen w-full'>
            <div className = 'overflow-scroll mt-10 mb-25 ml-10 w-[80%] bg-zinc-800 rounded-lg'></div>
-           <div className = ' min-w-20 ml-5 mt-10 mb-25 rounded-lg border-zinc-500 max-h-fit border-2 grow'> 
+           <div ref={ref} className = ' min-w-20 ml-5 mt-10 mb-25 rounded-lg border-zinc-500 max-h-fit border-2 grow'> 
            {queryItems.map((item)=>
            <>
-           <Draggable position={position} onStop={handleStop}><div className="flex hover:text-blue-500 items-center ml-2 font-semibold opacity-75 h-10 w-full text-white cursor-pointer z-50 ">{item}</div></Draggable>
+           <Draggable onDrag={handleDrag} position={position} onStop={handleStop}><div data-value= {item.endpoint} className="flex hover:text-blue-500 items-center ml-2 font-semibold opacity-75 h-10 w-full text-white cursor-pointer z-50 ">{item.text}</div></Draggable>
            <div className = 'border-b-2 border-slate-500'> </div>
            </>
            )}
