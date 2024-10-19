4a7cc5d83dd92f303443f824f0ee6631058806a6
SuperMicah585 <micahphlps@gmail.com>
2024-10-19 16:48:25 -0700

    stuff

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -63,7 +63,7 @@ scrollElementCallBack(e.target.innerText)
 
     return(
     
-        <div className='fixed flex w-full justify-center sm:justify-end sm:right-5 z-50 gap-5'>
+        <div className='fixed mt-1 flex w-full justify-center sm:justify-end sm:right-5 z-50 gap-5'>
      
         {items.map((header)=>
             <div 
