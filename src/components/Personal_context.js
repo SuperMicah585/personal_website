35703b18e4104974ab4789984a721c2891223a76
SuperMicah585 <micahphlps@gmail.com>
2024-10-19 16:38:31 -0700

    stuff

--- src/components/Personal_context.js
+++ src/components/Personal_context.js
@@ -24,7 +24,7 @@ const textArray = [
         <GifComponent/>
   
         <div className = ' absolute h-screen w-content flex items-center justify-center top-0 left-0'>
-        <div ref={ref} className = 'p-10 relative w-screen text-8xl sm:text-9xl font-monet left-0 flex flex-col items-center gap-20 text-blue-300 font-bold opacity-75'>   
+        <div ref={ref} className = 'p-10 relative w-screen text-8xl sm:text-9xl font-monet left-0 flex flex-col items-center gap-20 text-blue-300 font-black opacity-60'>   
 
             <div className = 'w-full flex justify-center lg:justify-start'> HEY, </div>
 
