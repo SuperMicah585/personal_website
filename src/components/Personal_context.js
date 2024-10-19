331992948482c0bbc77ce541ba1f2746980321ab
SuperMicah585 <micahphlps@gmail.com>
2024-10-19 16:39:53 -0700

    stuff

--- src/components/Personal_context.js
+++ src/components/Personal_context.js
@@ -26,11 +26,11 @@ const textArray = [
         <div className = ' absolute h-screen w-content flex items-center justify-center top-0 left-0'>
         <div ref={ref} className = 'p-10 relative w-screen text-8xl sm:text-9xl font-monet left-0 flex flex-col items-center gap-20 text-blue-300 font-black opacity-60'>   
 
-            <div className = 'w-full flex justify-center lg:justify-start'> HEY, </div>
+            <div className = 'w-full flex justify-center xl:justify-start'> HEY, </div>
 
             <div className='w-full flex justify-center' > I'M</div>
 
-            <div className='w-full flex justify-center lg:justify-end' > MICAH! </div>
+            <div className='w-full flex justify-center xl:justify-end' > MICAH! </div>
 
             </div>
 
