71544649dbf497cd31dbeb1a45467f9a545762bf
SuperMicah585 <micahphlps@gmail.com>
2024-10-22 23:31:17 -0700

    stuf

--- src/components/Personal_context.js
+++ src/components/Personal_context.js
@@ -4,16 +4,6 @@ const Personal_Context = forwardRef(({windowWidth}, ref) =>{
 
   const [musicON, setMusicOn] = useState(false)
 
-  const musicSVG = (boolean) =>{
-
-    return(boolean?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-    </svg>:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-</svg>
-)
-}
-
 const handleMusicClick = () =>{
    if(!musicON){
    togglePlayPause()
@@ -54,7 +44,7 @@ const handleMusicClick = () =>{
         <div ref={ref}  className = ' absolute h-screen w-content flex items-center justify-center top-0 left-0'>
       <div style={{
   textShadow: '-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black'
-}} className = {`p-10 duration-[10000ms] ${musicON?'opacity-0':'opacity-50'} relative w-screen text-8xl sm:text-9xl font-monet left-0 flex flex-col items-center gap-20 text-blue-500 font-black`}>   
+}} className = {`p-10 transition-opacity duration-[10000ms] ${musicON?'opacity-0':'opacity-50'} relative w-screen text-8xl sm:text-9xl font-monet left-0 flex flex-col items-center gap-20 text-blue-500 font-black`}>   
 
             <div className = 'w-full flex justify-center xl:justify-start'> HEY, </div>
 
@@ -72,7 +62,7 @@ const handleMusicClick = () =>{
 
 
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="size-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="size-9">
   <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
   <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
 </svg>
